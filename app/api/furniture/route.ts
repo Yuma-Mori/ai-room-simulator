62801df0dbdc4a07bc804684841daa63afa525b0
Yuma-Mori <fultonclip@gmail.com>
2025-06-19 11:41:05 +0000

    dbtest

--- app/api/furniture/route.ts
+++ app/api/furniture/route.ts
@@ -3,22 +3,24 @@ import { NextResponse } from 'next/server';
 import { Connector } from '@google-cloud/cloud-sql-connector';
 import mysql from 'mysql2/promise';
 
+const cloudSqlConnectionName = "roomsimulator-460108:asia-northeast1:room-simulator-db"
+
 export async function GET() {
   await new Promise(resolve => setTimeout(resolve, 100));
   const connector = new Connector();
-    const clientOpts = await connector.getOptions({
-        instanceConnectionName: "roomsimulator-460108:asia-northeast1:room-simulator-db"
-    });
-    const pool = mysql.createPool({
-        ...clientOpts,
-        user: "root",
-        password: process.env.DB_PASS,
-        database: "room_simulator",
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
-    });
-    const furnitureData = await pool.query('SELECT * FROM products');
+  const clientOpts = await connector.getOptions({
+      instanceConnectionName: cloudSqlConnectionName
+  });
+  const pool = mysql.createPool({
+      ...clientOpts,
+      user: "root",
+      password: process.env.DB_PASS,
+      database: "room_simulator",
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+  });
+  const furnitureData = await pool.query('SELECT * FROM products');
 
   return NextResponse.json({
     success: true,
