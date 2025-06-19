168dce61048edaa2d953cdb3db1cdf5f86fa957b
Yuma-Mori <fultonclip@gmail.com>
2025-06-19 14:04:37 +0000

    try

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -6,32 +6,40 @@ import mysql from 'mysql2/promise';
 const cloudSqlConnectionName = "***REMOVED***"
 
 export async function GET() {
-  const connector = new Connector();
-  const clientOpts = await connector.getOptions({
-      instanceConnectionName: cloudSqlConnectionName
-  });
-  const pool = mysql.createPool({
-      ...clientOpts,
-      user: "root",
-      password: process.env.DB_PASS,
-      database: "room_simulator",
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-  });
-  const furnitureData = await pool.query('SELECT * FROM products');
-  console.log("furnitureData", furnitureData);
-  if (!furnitureData ) {
-    return NextResponse.json({
-    success: true,
-    data: sampleData,
-    total: sampleData.length
-  });
-  }
+  try {
+    const connector = new Connector();
+    const clientOpts = await connector.getOptions({
+        instanceConnectionName: cloudSqlConnectionName
+    });
+    const pool = mysql.createPool({
+        ...clientOpts,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "room_simulator",
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+    const furnitureData = await pool.query('SELECT * FROM products');
+    console.log("furnitureData", furnitureData);
+    if (!furnitureData ) {
+      return NextResponse.json({
+      success: true,
+      data: sampleData,
+      total: sampleData.length
+    });
+    }
 
-  return NextResponse.json({
-    success: true,
-    data: furnitureData,
-    total: furnitureData.length
-  });
+    return NextResponse.json({
+      success: true,
+      data: furnitureData,
+      total: furnitureData.length
+    });
+  } catch (err) {
+    console.error('DB fetch error:', err);
+    return NextResponse.json({
+      success: false,
+      error: 'DB fetch failed',
+    });
+  }
 }
