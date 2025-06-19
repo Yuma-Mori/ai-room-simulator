aefcb66d318d54b49d6dfd25564c0599663d1c3c
Yuma-Mori <fultonclip@gmail.com>
2025-06-19 13:02:04 +0000

    test

--- app/api/furniture/route.ts
+++ app/api/furniture/route.ts
@@ -1,26 +1,33 @@
 import { NextResponse } from 'next/server';
-import { furnitureData } from '../../../data/furniture';
+import { furnitureData as sampleData } from '../../../data/furniture';
 import { Connector } from '@google-cloud/cloud-sql-connector';
 import mysql from 'mysql2/promise';
 
 const cloudSqlConnectionName = "***REMOVED***"
 
 export async function GET() {
-  // const connector = new Connector();
-  // const clientOpts = await connector.getOptions({
-  //     instanceConnectionName: cloudSqlConnectionName
-  // });
-  // const pool = mysql.createPool({
-  //     ...clientOpts,
-  //     user: "root",
-  //     password: process.env.DB_PASS,
-  //     database: "room_simulator",
-  //     waitForConnections: true,
-  //     connectionLimit: 10,
-  //     queueLimit: 0
-  // });
-  // const furnitureData = await pool.query('SELECT * FROM products');
-  // console.log("furnitureData", furnitureData);
+  const connector = new Connector();
+  const clientOpts = await connector.getOptions({
+      instanceConnectionName: cloudSqlConnectionName
+  });
+  const pool = mysql.createPool({
+      ...clientOpts,
+      user: "root",
+      password: process.env.DB_PASS,
+      database: "room_simulator",
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+  });
+  const furnitureData = await pool.query('SELECT * FROM products');
+  console.log("furnitureData", furnitureData);
+  if (!furnitureData ) {
+    return NextResponse.json({
+    success: true,
+    data: sampleData,
+    total: sampleData.length
+  });
+  }
 
   return NextResponse.json({
     success: true,
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import { useEffect, useState } from "react";
 import Header from "@/components/organisms/Header";
 import Footer from "@/components/molecules/Footer";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
