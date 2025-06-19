5b3f0a4045fc72f12d5622810626adad4f97c1ae
Yuma-Mori <fultonclip@gmail.com>
2025-06-19 14:53:05 +0000

    try

---
+++ data/furniture.ts
@@ -0,0 +1,17 @@
+import { Furniture } from '../types/furniture';
+
+export const furnitureData: Furniture[] = [
+  {
+    id: '1',
+    name: 'モダンチェア',
+    description: 'シンプルでスタイリッシュなモダンチェア。',
+    price: 15000, // 円
+    category: '椅子',
+    inStock: true,
+    image: '/images/chair1.jpg',
+    width: 45,
+    height: 85,
+    depth: 50,
+    model: 'MC-2023',
+  }
+]
